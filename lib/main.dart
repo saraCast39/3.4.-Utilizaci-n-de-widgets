aec5b94f1db09f73dbfa30888bf87025ea5942fd
saracastro <saritaycloe@hotmail.com>
2025-10-26 17:05:45 -0600

    Update main.dart: add Hello World and center text

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,53 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Flutter Widgets Demo',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
           centerTitle: true,
         ),
-        body: const Center(
-          child: Text(
-            'Hello World',
-            style: TextStyle(fontSize: 24), 
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center, // centra verticalmente
+            children: [
+              
+              Container(
+                color: Colors.lightGreen,
+                padding: const EdgeInsets.all(16),
+                child: const Text(
+                  'Hello World',
+                  style: TextStyle(fontSize: 24, color: Colors.white),
+                ),
+              ),
+
+              const SizedBox(height: 30),
+
+              
+              
+              
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(width: 150, height: 150, color: Colors.lightGreenAccent),
+                  const Text(
+                    'Hola',
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ],
+              ),
+Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  SizedBox(width: 8),
+                  Text('Bienvenidos'),
+                ],
+              ),
+
+              const SizedBox(height: 30),
+
+
+            ],
           ),
         ),
       ),
